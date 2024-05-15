b703273efd091f30a09d000025c3c635bf9dd55d
Quivin19 <136719825+Quivin19@users.noreply.github.com>
2024-05-15 09:50:26 -0600

    Creacion pedidos

--- Api/models/handler/pedido_handler.php
+++ Api/models/handler/pedido_handler.php
@@ -2,114 +2,129 @@
 // Se incluye la clase para trabajar con la base de datos.
 require_once('../../helpers/database.php');
 /*
-*	Clase para manejar el comportamiento de los datos de las tablas PEDIDO y DETALLE_PEDIDO.
+*	Clase para manejar el comportamiento de los datos de la tabla PRODUCTO.
 */
-class PedidoHandler
+class ProductoHandler
 {
     /*
     *   Declaración de atributos para el manejo de datos.
     */
-    protected $id_pedido = null;
-    protected $id_detalle = null;
-    protected $cliente = null;
-    protected $producto = null;
-    protected $cantidad = null;
+    protected $id = null;
+    protected $nombre = null;
+    protected $descripcion = null;
+    protected $precio = null;
+    protected $existencias = null;
+    protected $imagen = null;
+    protected $categoria = null;
     protected $estado = null;
 
-    /*
-    *   ESTADOS DEL PEDIDO
-    *   Pendiente (valor por defecto en la base de datos). Pedido en proceso y se puede modificar el detalle.
-    *   Finalizado. Pedido terminado por el cliente y ya no es posible modificar el detalle.
-    *   Entregado. Pedido enviado al cliente.
-    *   Anulado. Pedido cancelado por el cliente después de ser finalizado.
-    */
+    // Constante para establecer la ruta de las imágenes.
+    const RUTA_IMAGEN = '../../images/productos/';
 
     /*
     *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
     */
-    // Método para verificar si existe un pedido en proceso con el fin de iniciar o continuar una compra.
-    public function getOrder()
+    public function searchRows()
     {
-        $this->estado = 'Pendiente';
-        $sql = 'SELECT id_pedido
-                FROM pedido
-                WHERE estado_pedido = ? AND id_cliente = ?';
-        $params = array($this->estado, $_SESSION['idCliente']);
-        if ($data = Database::getRow($sql, $params)) {
-            $_SESSION['idPedido'] = $data['id_pedido'];
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Método para iniciar un pedido en proceso.
-    public function startOrder()
-    {
-        if ($this->getOrder()) {
-            return true;
-        } else {
-            $sql = 'INSERT INTO pedido(direccion_pedido, id_cliente)
-                    VALUES((SELECT direccion_cliente FROM cliente WHERE id_cliente = ?), ?)';
-            $params = array($_SESSION['idCliente'], $_SESSION['idCliente']);
-            // Se obtiene el ultimo valor insertado de la llave primaria en la tabla pedido.
-            if ($_SESSION['idPedido'] = Database::getLastRow($sql, $params)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    // Método para agregar un producto al carrito de compras.
-    public function createDetail()
-    {
-        // Se realiza una subconsulta para obtener el precio del producto.
-        $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad_producto, id_pedido)
-                VALUES(?, (SELECT precio_producto FROM producto WHERE id_producto = ?), ?, ?)';
-        $params = array($this->producto, $this->producto, $this->cantidad, $_SESSION['idPedido']);
-        return Database::executeRow($sql, $params);
-    }
-
-    // Método para obtener los productos que se encuentran en el carrito de compras.
-    public function readDetail()
-    {
-        $sql = 'SELECT id_detalle, nombre_producto, detalle_pedido.precio_producto, detalle_pedido.cantidad_producto
-                FROM detalle_pedido
-                INNER JOIN pedido USING(id_pedido)
-                INNER JOIN producto USING(id_producto)
-                WHERE id_pedido = ?';
-        $params = array($_SESSION['idPedido']);
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, nombre_categoria, estado_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE nombre_producto LIKE ? OR descripcion_producto LIKE ?
+                ORDER BY nombre_producto';
+        $params = array($value, $value);
         return Database::getRows($sql, $params);
     }
 
-    // Método para finalizar un pedido por parte del cliente.
-    public function finishOrder()
+    public function readAll()
     {
-        $this->estado = 'Finalizado';
-        $sql = 'UPDATE pedido
-                SET estado_pedido = ?
-                WHERE id_pedido = ?';
-        $params = array($this->estado, $_SESSION['idPedido']);
+        $sql = 'SELECT 
+        dp.id_detalle AS id_detalle,
+        p.imagen_producto AS imagen_producto,
+        p.nombre_producto AS nombre_producto,
+        c.nombre_cliente AS nombre_cliente,
+        c.direccion_cliente AS direccion_cliente,
+        pe.estado_pedido AS estado_pedido
+    FROM 
+        detalle_pedido dp
+    JOIN 
+        producto p ON dp.id_producto = p.id_producto
+    JOIN 
+        pedido pe ON dp.id_pedido = pe.id_pedido
+    JOIN 
+        cliente c ON pe.id_cliente = c.id_cliente;';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto
+                FROM producto
+                WHERE id_producto = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function readFilename()
+    {
+        $sql = 'SELECT imagen_producto
+                FROM producto
+                WHERE id_producto = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM producto
+                WHERE id_producto = ?';
+        $params = array($this->id);
         return Database::executeRow($sql, $params);
     }
 
-    // Método para actualizar la cantidad de un producto agregado al carrito de compras.
-    public function updateDetail()
+    public function readProductosCategoria()
     {
-        $sql = 'UPDATE detalle_pedido
-                SET cantidad_producto = ?
-                WHERE id_detalle = ? AND id_pedido = ?';
-        $params = array($this->cantidad, $this->id_detalle, $_SESSION['idPedido']);
-        return Database::executeRow($sql, $params);
+        $sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE id_categoria = ? AND estado_producto = true
+                ORDER BY nombre_producto';
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
     }
 
-    // Método para eliminar un producto que se encuentra en el carrito de compras.
-    public function deleteDetail()
+    /*
+    *   Métodos para generar gráficos.
+    */
+    public function cantidadProductosCategoria()
     {
-        $sql = 'DELETE FROM detalle_pedido
-                WHERE id_detalle = ? AND id_pedido = ?';
-        $params = array($this->id_detalle, $_SESSION['idPedido']);
-        return Database::executeRow($sql, $params);
+        $sql = 'SELECT nombre_categoria, COUNT(id_producto) cantidad
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY cantidad DESC LIMIT 5';
+        return Database::getRows($sql);
+    }
+
+    public function porcentajeProductosCategoria()
+    {
+        $sql = 'SELECT nombre_categoria, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM producto)), 2) porcentaje
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY porcentaje DESC';
+        return Database::getRows($sql);
+    }
+
+    /*
+    *   Métodos para generar reportes.
+    */
+    public function productosCategoria()
+    {
+        $sql = 'SELECT nombre_producto, precio_producto, estado_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE id_categoria = ?
+                ORDER BY nombre_producto';
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
     }
 }

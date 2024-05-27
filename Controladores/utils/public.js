5da601e8a95daed4163fcbc5afc1392a6ff4b387
Juan2022-a <20220671@ricaldone.edu.sv>
2024-05-27 12:02:24 -0600

    PUBLICA, diseño y codificación de register y mas
    Se creo lo necesario para el registro de cliente(falta revisar si los campos son los de la base), creación de card que mandan a llamar las categorias.

--- Controladores/utils/public.js
+++ Controladores/utils/public.js
@@ -11,7 +11,7 @@ MAIN.style.paddingTop = '75px';
 MAIN.style.paddingBottom = '100px';
 MAIN.classList.add('container');
 // Se establece el título de la página web.
-document.querySelector('title').textContent = 'CoffeeShop - Store';
+document.querySelector('title').textContent = 'PrettyUsine - Store';
 // Constante para establecer el elemento del título principal.
 const MAIN_TITLE = document.getElementById('mainTitle');
 MAIN_TITLE.classList.add('text-center', 'py-3');
@@ -32,7 +32,7 @@ const loadTemplate = async () => {
                 <header>
                     <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                         <div class="container">
-                            <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="50" alt="CoffeeShop"></a>
+                            <a class="navbar-brand" href="index.html"><img src="../../Recursos/Img/LogoPretty.png" height="50" alt="PrettyUsine"></a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
@@ -56,7 +56,7 @@ const loadTemplate = async () => {
             <header>
                 <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                     <div class="container">
-                        <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="50" alt="CoffeeShop"></a>
+                        <a class="navbar-brand" href="index.html"><img src="../../Recursos/Img/LogoPretty.png" height="50" alt="PrettyUsine"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -78,12 +78,12 @@ const loadTemplate = async () => {
             <nav class="navbar fixed-bottom bg-body-tertiary">
                 <div class="container">
                     <div>
-                        <h6>CoffeeShop</h6>
-                        <p><i class="bi bi-c-square"></i> 2018-2024 Todos los derechos reservados</p>
+                        <h6>PrettyUsine</h6>
+                        <p><i class="bi bi-c-square"></i> 2023-2024 Todos los derechos reservados</p>
                     </div>
                     <div>
                         <h6>Contáctanos</h6>
-                        <p><i class="bi bi-envelope"></i> dacasoft@outlook.com</p>
+                        <p><i class="bi bi-envelope"></i> prettyusine@gmail.com</p>
                     </div>
                 </div>
             </nav>
